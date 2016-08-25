47eb67ea362facf241ba98343553056e235d7e79
Jacob Bednarz <jacob.bednarz@gmail.com>
2016-08-26 08:37:26 +1000

    Deprecate in favour of brewcask
    As mentioned in boxen/boxen#203 brewcask[1] does better job of managing
    simple application installations like this so we are deprecating it in
    favour of using Boxen's built in support[2] for homebrew and brewcask.
    
    [1]: https://caskroom.github.io/
    [2]: https://github.com/boxen/puppet-boxen/blob/master/manifests/personal.pp#L14-L17

--- manifests/init.pp
+++
@@ -1,9 +0,0 @@
-# Install Coccinellida.
-class coccinellida (
-  $version = '0.6.1',
-) {
-  package { 'Coccinellida':
-    provider => 'compressed_app',
-    source   => "http://hivelocity.dl.sourceforge.net/project/coccinellida/Coccinellida-${version}.zip",
-  }
-}
--- spec/classes/coccinellida_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-describe 'coccinellida' do
-  it do
-    should contain_class('coccinellida')
-    should contain_package('Coccinellida').with_provider('compressed_app')
-    should contain_package('Coccinellida').with_source('http://hivelocity.dl.sourceforge.net/project/coccinellida/Coccinellida-0.6.1.zip')
-  end
-end
